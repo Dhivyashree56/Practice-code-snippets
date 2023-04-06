81d21397599530af1c53d34f02ab3cca83e18c57
Dhivyashree56 <dhivyashree@rently.com>
2023-04-06 15:50:52 +0530

    test

--- MochaTest/test/appTest.js
+++ MochaTest/test/appTest.js
@@ -2,34 +2,33 @@ const assert = require('chai').assert;
 const sinon = require('sinon'); // import sinon package
 const app = require('../app');
 
-describe('App',function(){
-    it('sayHello should return hello', function(){
-        var result=app.sayHello();
-        assert.equal(result,"hello");
+describe('App', function () {
+    it('sayHello should return hello', function () {
+        var result = app.sayHello();
+        assert.equal(result, "hello");
     })
 
-    it('sayHello should return type string', function(){
-        var result=app.sayHello();
-        assert.typeOf(result,'string');
+    it('sayHello should return type string', function () {
+        var result = app.sayHello();
+        assert.typeOf(result, 'string');
     })
 
-    it('addResult should return 2', function(){
-        var result=app.addResult(1,1);
-        assert.equal(result,2);
+    it('addResult should return 2', function () {
+        var result = app.addResult(1, 1);
+        assert.equal(result, 2);
     })
 
-    it('addResult should return type number', function(){
-        var result=app.addResult();
-        assert.typeOf(result,'number');
-        /*const clock = sinon.useFakeTimers(); // create a fake clock
-        console.log('clock', clock.now);
-        setTimeout(function(){
-            console.log('Timer executed');
-            
-        },100);
-        clock.tick(100); // advance the clock by 100ms
-        console.log('clock', clock.now); // clock 100
-        clock.tick(200); // advance the clock by 200ms
-        console.log('clock', clock.now); // clock 200 */
+    it('addResult should return type number', function () {
+        var result = app.addResult();
+        assert.typeOf(result, 'number');
+        
+        // const clock = sinon.useFakeTimers(); // create a fake clock
+        // console.log('clock', clock.now);
+        // console.log('--------------')
+        // setTimeout(function () {
+        //     console.log('Timer executed');
+        // }, 5000);
+        // clock.tick(5000); // advance the clock by 5000ms
+        // console.log('clock', clock.now); // clock 5000
     })
 })
